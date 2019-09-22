82cf4455973ebab6a2862039392927874bec94c3
deezy13 <deezy13@gmail.com>
2019-09-22 20:02:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,9 @@ def load_library(file_path)
   emoticon = YAML.load_file(file_path)
   
   emoji_dir = Hash.new 
+  emoji_dir[:get_meaning] = {}
+  emoji_dir[:get_emoticon] = {}
+  emoji_dir
 end 
 
 def get_japanese_emoticon
