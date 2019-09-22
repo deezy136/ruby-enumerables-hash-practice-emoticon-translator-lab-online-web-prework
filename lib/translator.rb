78ee33ac3ba4fc1d8c54dc6d5fe58b6d7562ab87
deezy13 <deezy13@gmail.com>
2019-09-22 20:22:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,8 +14,8 @@ def load_library(file_path)
   
   emoticons.each do |english_word, emoticon_set |
     
-    emoji_dir["get_meaning"][emoticon_set.first] = all_other_levels.last
-    emoji_dir["get_emoticon"][all_other_levels.last] = first_level
+    emoji_dir["get_meaning"][emoticon_set.first] = emoticon_set.last
+    emoji_dir["get_emoticon"][emoticon_set.last] = english_word
    
    
   end 
