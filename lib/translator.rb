9e451167b8cdb7f88ca16e6adf080eeba5df41e0
deezy13 <deezy13@gmail.com>
2019-09-22 20:00:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,13 +8,6 @@ def load_library(file_path)
   # code goes here
   emoticon = YAML.load_file(file_path)
   
-  emoticon.each do |first_tier, other_tiers|
-  
-  
-end
-end 
-p "it worked"
-return "it worked"
 end 
 
 def get_japanese_emoticon
